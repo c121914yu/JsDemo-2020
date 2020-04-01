40328308c7f72b24a12323088b33643cf48a1d7a
yjl <545436317@qq.com>
2020-04-01 15:28:00 +0800

    电影座位预定

--- README.md
+++ README.md
@@ -1,12 +1,46 @@
-# From表单验证
+# 电影票预定
+
+```html
+<!-- 选择器 -->
+<select id="">
+	<option value=""></option>
+</select>
+```
 
 ```css
-/* 隐藏与显示 */
-visibility: hidden;
-visibility: visible;
+/* 同类选择器:nth-child,nth-last-child*/
+:nth-child(2) /*第二个*/
+:nth-last-child(2) /*倒数第二个*/
+
+/* 反向选择 */
+:not(.occupied) /* 不包含occipied的类 */
+
+/* 3D效果 */
+perspective: 1000px; /* 3D视距*/
+transform: rotateX(-45deg); /* X轴旋转*/
 ```
 
 ```js
-// 邮箱验证正则表达式
-const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+// 本地永久存储
+localStorage.getItem(key)
+localStorage.setItem(key,string)
+
+// 关于点击事件
+dom.onclick = (e) => {
+	e.target // 可直接获取到被点击的子元素
+}
+
+// dom类切换
+dom.classList.toggle(name) // 包含name则去掉，不包含则添加
+
+// 通过展开运算符将类数组转化成数组
+const selectedSeats = document.querySelectorAll('.row .seat.selected')//类数组
+const seats = [...selectedSeats] //数组
+
+// 下拉框改变事件
+select.onchange = (e) => {
+	e.target.selectedIndex //选中的下标
+	+e.target.value //选中的value值，+号是字符串转数字的隐式转化
+}
+select.selectedIndex = number //手动切换下拉框选中值，不会触发onchange
 ```
--- index.js
+++ index.js
@@ -1,73 +1,63 @@
 // 获取dom
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const container = document.querySelector('.container')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+let movieIndex = 0
+let ticketPrice = +movieSelect.value
 
-const message = 
-
-// 时间监听
-form.addEventListener('submit',(e) => {
-	e.preventDefault()
-	checkSpace([
-		{input:username,message:'用户名不能为空'},
-		{input:email,message:'邮箱不能为空'},
-		{input:password,message:'请输入密码'},
-		{input:password2,message:'请再次输入密码'}
-	])
-	
-	checkEmail()
-	checkLength(username,3,15,'用户名')
-	checkLength(password,6,12,'密码')
-	checkEquality(password,password2,'两次密码不一致')
-})
-
-function checkSpace(inputArr){
-	inputArr.forEach(item => {
-		const input = item.input
-		if(input.value === "")
-			showError(input,item.message)
-		else
-			showSuccess(input)
+// 读取历史记录
+if(localStorage.getItem('selectIndex')){
+	const storage = JSON.parse(localStorage.getItem('selectIndex'))
+	console.log(storage)
+	movieSelect.selectedIndex = storage.movieIndex
+	movieIndex = storage.movieIndex
+	ticketPrice = storage.ticketPrice
+	seats.forEach((seat,index) => {
+		if(storage.seatsIndex.indexOf(index) > -1)
+			seat.classList.add('selected')
 	})
-}
-function checkEmail(){
-	if(email.value === '') return
-	const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
-	if(!reg.test(email.value))
-		showError(email,'邮箱格式错误')
-	else
-		showSuccess(email)
-}
-function checkLength(input,min,max,keyword){
-	const val = input.value
-	if(val === '') return
-	console.log(val.length > max)
-	if(val.length < min)
-		showError(input,`${keyword}长度不能小于${min}`)
-	else if(val.length > max)
-		showError(input,`${keyword}长度不能大于${max}`)
-}
-function checkEquality(input1,input2,message){
-	console.log()
-	if(input1.value === input2.value)
-		showSuccess(input2)
-	else
-		showError(input2,message)
+	updateSelected()
 }
 
-
-function showError(input,message){
-	const formControl = input.parentElement
-	const small = formControl.querySelector('small')
-	small.innerText = message
-	formControl.classList.remove('success')
-	formControl.classList.add('error')
+// 监听点击座位
+container.onclick = (e) => {
+	const seat = e.target
+	// 判断是否是不可选
+	if(seat.classList.contains('seat') && !seat.classList.contains('occupied')){
+		// 切换selected属性，包含则删除，不包含则添加
+		seat.classList.toggle('selected')
+		// 更新座位数和票价
+		updateSelected()
+	}
+}
+// 监听下拉框
+movieSelect.onchange = (e) => {
+	// 下拉框的index
+	movieIndex = e.target.selectedIndex
+	// 票价
+	ticketPrice = +e.target.value
+	seats.forEach(seat => {
+		seat.classList.remove('selected')
+	})
+	updateSelected()
 }
 
-function showSuccess(input){
-	const formControl = input.parentElement
-	formControl.classList.remove('error')
-	formControl.classList.add('success')
+function updateSelected(){
+	const selectedSeats = document.querySelectorAll('.row .seat.selected')
+	
+	// 通过展开运算符将类数组转化成数组
+	const seatsIndex = [...selectedSeats].map(seat => {
+		return [...seats].indexOf(seat)
+	})
+	localStorage.setItem('selectIndex',JSON.stringify({
+		movieIndex,
+		ticketPrice,
+		seatsIndex,
+	}))
+	
+	
+	count.innerText = selectedSeats.length
+	total.innerText = selectedSeats.length * ticketPrice
 }

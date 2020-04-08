49762bbf3dc756f90bad14935d4697591ff44efc
yjl <545436317@qq.com>
2020-04-08 11:00:59 +0800

    汇率计算器

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
-# 自定义视频播放器
+# 实时汇率计算器
 
-1. 进度条样式自定义，参考progress
-2. video API
+1. 原生js fetch请求
+2. exchangerate汇率API
 
-> **事件**
-> play - 监听播放
-> pause - 监听暂停
-> timeupdate - 监听时间更新
-
-> **方法**
-> play() - 播放
-> pause() - 暂停
-
-> **属性**
-> currentTime - 当前播放的时间
-> duration - 视频总时长
+```js
+fetch(url)
+.then(res => res.json()) //等价于return res.json()
+.then(data => {
+	
+})
+```
--- index.js
+++ index.js
@@ -1,56 +1,34 @@
-// 获取节点
-const video = document.getElementById('video')
-const play = document.getElementById('play')
-const stop = document.getElementById('stop')
-const progress = document.getElementById('progress')
-const time = document.getElementById('time')
+// 获取dom
+const currency1 = document.getElementById("currency-one")
+const amount1 = document.getElementById('amount-one')
+const currency2 = document.getElementById("currency-two")
+const amount2 = document.getElementById('amount-two')
+const swap = document.getElementById('swap')
+const rateEl = document.getElementById('rate')
 
-function changeStatus(){
-	if(video.paused)
-		video.play()
-	else
-		video.pause()
+// 获取汇率
+function calculate(){
+	const val1 = currency1.value
+	const val2 = currency2.value
+	
+	fetch(`https://api.exchangerate-api.com/v4/latest/${val1}`)
+	.then(res => res.json())
+	.then(data => {
+		const rate = data.rates[val2]
+		rateEl.innerText = `1${val1} = ${rate}${val2}`
+		amount2.value = (amount1.value * rate).toFixed(2)
+	})
 }
+calculate()
 
-// 添加事件
-// 点击视频播放/暂停
-video.onclick = changeStatus
-// 视频暂停
-video.onpause = () => {
-	play.innerHTML = 
-		`
-			<i class="fa fa-play fa-2x"></i>
-		`
-}
-// 视频播放
-video.onplay = () => {
-	play.innerHTML =
-		`
-			<i class="fa fa-pause fa-2x"></i>
-		`
-}
+// 事件监听
+currency1.onchange = calculate
+amount1.oninput = calculate
+currency2.onchange = calculate
 
-// 监听播放时间
-video.ontimeupdate = (e) => {
-	const currentTime = video.currentTime
-	progress.value = (currentTime/video.duration) * 100
-	let mins = Math.floor(currentTime / 60)
-	if(mins < 10)
-		mins = '0' + mins
-	let seconds = Math.floor(currentTime % 60)
-	if(seconds < 10)
-		seconds = '0' + seconds
-	time.innerText = `${mins}:${seconds}`
-}
-
-// 点击播放/暂停按键
-play.onclick = changeStatus
-//点击停止
-stop.onclick = () => {
-	video.pause()
-	video.currentTime = 0
-}
-// 拖动进度条
-progress.onchange = () => {
-	video.currentTime = +progress.value/100 * video.duration
+swap.onclick = () => {
+	const temp = currency1.value
+	currency1.value = currency2.value
+	currency2.value = temp
+	calculate()
 }

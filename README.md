f5c98448661d159c1b059d9835d48035f9fa0d03
yjl <545436317@qq.com>
2020-04-08 18:15:18 +0800

    数组方法

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# 实时汇率计算器
+# DOM方法
 
-1. 原生js fetch请求
-2. exchangerate汇率API
+1. 货币转化表达式
+2. sort排序
+3. reduce求和
 
 ```js
-fetch(url)
-.then(res => res.json()) //等价于return res.json()
-.then(data => {
-	
+// 货币转化表达式
+money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+
+//sort，返回值>0时会交换a,b.会修改原数组
+// 可以理解为冒泡排序，后者大就向前移动
+arr.sort((a,b) => {
+	return b - a//按降序
 })
+
+// reduce求和
+/*
+	sum : 和（最终返回的值）
+	num : 数组的值
+	init : 初始值
+*/
+arr.reduce((sum,num) => {
+	return sum += num
+},init)
 ```
--- index.js
+++ index.js
@@ -1,34 +1,79 @@
-// 获取dom
-const currency1 = document.getElementById("currency-one")
-const amount1 = document.getElementById('amount-one')
-const currency2 = document.getElementById("currency-two")
-const amount2 = document.getElementById('amount-two')
-const swap = document.getElementById('swap')
-const rateEl = document.getElementById('rate')
+// dom
+const main = document.getElementById('main')
+const addUser = document.getElementById('add-user')
+const Double = document.getElementById('double')
+const showMillionaires = document.getElementById('show-millionaires')
+const Sort = document.getElementById('sort')
+const Calculate = document.getElementById('caculate-wealth')
 
-// 获取汇率
-function calculate(){
-	const val1 = currency1.value
-	const val2 = currency2.value
-	
-	fetch(`https://api.exchangerate-api.com/v4/latest/${val1}`)
-	.then(res => res.json())
-	.then(data => {
-		const rate = data.rates[val2]
-		rateEl.innerText = `1${val1} = ${rate}${val2}`
-		amount2.value = (amount1.value * rate).toFixed(2)
+var persionArr = new Array()
+
+// 获取数据
+async function getRandomUser(){
+	const res = await fetch("https://randomuser.me/api")
+	const data = await res.json()
+	const user = data.results[0]
+	const newUser = {
+	  name: `${user.name.first} ${user.name.last}`,
+	  money: Math.floor(Math.random() * 1000000)
+	}
+	persionArr.push(newUser)
+	updataDOM()
+}
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+function updataDOM(){
+	main.innerHTML = " <h2><strong>Person</strong>Wealth</h2>"
+	persionArr.forEach(item => {
+		const element = document.createElement('div')
+		element.classList.add('person')
+		element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`
+		main.appendChild(element)
 	})
 }
-calculate()
-
-// 事件监听
-currency1.onchange = calculate
-amount1.oninput = calculate
-currency2.onchange = calculate
-
-swap.onclick = () => {
-	const temp = currency1.value
-	currency1.value = currency2.value
-	currency2.value = temp
-	calculate()
+// 转换货币格式
+function formatMoney(money){
+	return "$ " + money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
 }
+
+addUser.onclick = getRandomUser
+
+Double.onclick = () => {
+	 const newArr = persionArr.map(item => {
+		 return {
+			 name : item.name,
+			 money : item.money * 2
+		 }
+	 })
+	 persionArr = newArr
+	 updataDOM()
+}
+
+showMillionaires.onclick = () => {
+	const newArr = persionArr.filter(item => {
+		return item.money >= 1000000
+	})
+	persionArr = newArr
+	updataDOM()
+}
+
+Sort.onclick = () => {
+	// >0则交换顺序
+	persionArr.sort((a,b) => {
+		return b.money - a.money
+	})
+	updataDOM()
+}
+
+Calculate.onclick = () => {
+	let sum = persionArr.reduce((sum,item) => {
+		return sum += item.money
+	},0)
+	sum = formatMoney(sum)
+	const element = document.createElement('div')
+	element.innerHTML = `<h3>Total Wealth<strong>${sum}</strong></h3>`
+	main.appendChild(element)
+}
+

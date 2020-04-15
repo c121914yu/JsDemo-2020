f40569f34ca3612ddb8f725686dae0cadffe3ecd
yjl <545436317@qq.com>
2020-04-15 14:18:44 +0800

    折叠侧边栏

--- README.md
+++ README.md
@@ -1,26 +1,8 @@
-# DOM方法
-
-1. 货币转化表达式
-2. sort排序
-3. reduce求和
+# 折叠侧边栏
 
 ```js
-// 货币转化表达式
-money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
-
-//sort，返回值>0时会交换a,b.会修改原数组
-// 可以理解为冒泡排序，后者大就向前移动
-arr.sort((a,b) => {
-	return b - a//按降序
-})
-
-// reduce求和
-/*
-	sum : 和（最终返回的值）
-	num : 数组的值
-	init : 初始值
-*/
-arr.reduce((sum,num) => {
-	return sum += num
-},init)
+// 判断点击dom外
+window.onclick = (e) => {
+	e.target == modal ? modal.classList.remove('show-modal') : ''
+}
 ```
--- index.js
+++ index.js
@@ -1,79 +1,19 @@
 // dom
-const main = document.getElementById('main')
-const addUser = document.getElementById('add-user')
-const Double = document.getElementById('double')
-const showMillionaires = document.getElementById('show-millionaires')
-const Sort = document.getElementById('sort')
-const Calculate = document.getElementById('caculate-wealth')
+const toggle = document.getElementById('toggle')
+const close = document.getElementById('close')
+const open = document.getElementById('open')
+const modal = document.getElementById('modal')
 
-var persionArr = new Array()
-
-// 获取数据
-async function getRandomUser(){
-	const res = await fetch("https://randomuser.me/api")
-	const data = await res.json()
-	const user = data.results[0]
-	const newUser = {
-	  name: `${user.name.first} ${user.name.last}`,
-	  money: Math.floor(Math.random() * 1000000)
-	}
-	persionArr.push(newUser)
-	updataDOM()
-}
-getRandomUser()
-getRandomUser()
-getRandomUser()
-
-function updataDOM(){
-	main.innerHTML = " <h2><strong>Person</strong>Wealth</h2>"
-	persionArr.forEach(item => {
-		const element = document.createElement('div')
-		element.classList.add('person')
-		element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`
-		main.appendChild(element)
-	})
-}
-// 转换货币格式
-function formatMoney(money){
-	return "$ " + money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+toggle.onclick = () => {
+	document.body.classList.toggle('show-nav')
 }
 
-addUser.onclick = getRandomUser
-
-Double.onclick = () => {
-	 const newArr = persionArr.map(item => {
-		 return {
-			 name : item.name,
-			 money : item.money * 2
-		 }
-	 })
-	 persionArr = newArr
-	 updataDOM()
+open.onclick = () => {
+	modal.classList.add('show-modal')
 }
-
-showMillionaires.onclick = () => {
-	const newArr = persionArr.filter(item => {
-		return item.money >= 1000000
-	})
-	persionArr = newArr
-	updataDOM()
+close.onclick = () => {
+	modal.classList.remove('show-modal')
 }
-
-Sort.onclick = () => {
-	// >0则交换顺序
-	persionArr.sort((a,b) => {
-		return b.money - a.money
-	})
-	updataDOM()
+window.onclick = (e) => {
+	e.target == modal ? modal.classList.remove('show-modal') : ''
 }
-
-Calculate.onclick = () => {
-	let sum = persionArr.reduce((sum,item) => {
-		return sum += item.money
-	},0)
-	sum = formatMoney(sum)
-	const element = document.createElement('div')
-	element.innerHTML = `<h3>Total Wealth<strong>${sum}</strong></h3>`
-	main.appendChild(element)
-}
-

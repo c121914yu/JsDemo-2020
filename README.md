5dea2a34aa50c192c937f69882ca9378ed632f24
yjl <545436317@qq.com>
2020-04-15 18:16:00 +0800

    svg图使用

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
-# From表单验证
+# SVG 矢量图
+
+|        | 优点                                     | 缺点                 |
+| ------ | ---------------------------------------- | -------------------- |
+| svg    | 不依赖像素，放大后不失真，可直接绑定 dom | 动画性能低           |
+| canvas | 定制型强，动画效果好                     | 依赖像素，绘制较复杂 |
+
+```html
+<svg height="250" width="200" class="figure-container">
+	<!-- 起点坐标，终点坐标 -->
+	<line x1="60" y1="20" x2="140" y2="20" />
+	<!-- 圆心坐标，半径 -->
+	<circle cx="140" cy="70" r="20" />
+</svg>
+```
 
 ```css
-/* 隐藏与显示 */
-visibility: hidden;
-visibility: visible;
+/* svg */
+/* 填充 */
+fill: transparent;
+/* 边框 */
+stroke: #fff;
+/* 线宽度 */
+stroke-width: 4px;
+/* 边缘效果 */
+stroke-linecap: round;
 ```
 
 ```js
-// 邮箱验证正则表达式
-const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+// 判断arr是否包含item
+arr.include(item)
+// 键盘事件
+e.key // 获取输入的键名
+e.keyCode // 键值（数字）
 ```
--- index.js
+++ index.js
@@ -1,73 +1,105 @@
-// 获取dom
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+// dom获取
+const wordEl = document.getElementById("word")
+const errLetter = document.getElementById("error-letters")
+const noteContainer = document.getElementById("note-container")
+const popup = document.getElementById("popup-contaniner")
+const finalMessage = document.getElementById("final-message")
+const playAgain = document.getElementById("play-btn")
 
-const message = 
+const partArr = document.querySelectorAll(".figure")
 
-// 时间监听
-form.addEventListener('submit',(e) => {
-	e.preventDefault()
-	checkSpace([
-		{input:username,message:'用户名不能为空'},
-		{input:email,message:'邮箱不能为空'},
-		{input:password,message:'请输入密码'},
-		{input:password2,message:'请再次输入密码'}
-	])
-	
-	checkEmail()
-	checkLength(username,3,15,'用户名')
-	checkLength(password,6,12,'密码')
-	checkEquality(password,password2,'两次密码不一致')
-})
+const words = [
+	"application", "programming", "intertface", "wonder"
+]
+var selectedWord = words[Math.floor(Math.random() * words.length)]
 
-function checkSpace(inputArr){
-	inputArr.forEach(item => {
-		const input = item.input
-		if(input.value === "")
-			showError(input,item.message)
-		else
-			showSuccess(input)
+var correctLetters = []
+var wrongLetters = []
+
+// 显示单词函数
+function displayWord() {
+	wordEl.innerHTML =
+		`
+		${selectedWord.split("").map(letter => 
+			`
+				<span class="letter">
+					${correctLetters.includes(letter) ? letter : ""}
+				</span>
+			`
+		).join("")}
+	`
+	const innerWord = wordEl.innerText.replace(/\n/g, "")
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = "恭喜你输入正确"
+		popup.style.display = "flex"
+	}
+}
+displayWord()
+
+// 显示提示框函数
+function showNote() {
+	noteContainer.classList.add("show")
+	setTimeout(() => {
+		noteContainer.classList.remove("show")
+	}, 2000);
+}
+
+// 更新错误内容
+function updateWrongLettersEl() {
+	errLetter.innerHTML =
+		`
+		<p>错误：</p>
+		<span>${wrongLetters.join(',')}</span>
+	`
+
+	// 火柴人
+	partArr.forEach((item, index) => {
+		const errTimes = wrongLetters.length
+		if (index < errTimes)
+			item.style.display = "block"
 	})
-}
-function checkEmail(){
-	if(email.value === '') return
-	const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
-	if(!reg.test(email.value))
-		showError(email,'邮箱格式错误')
-	else
-		showSuccess(email)
-}
-function checkLength(input,min,max,keyword){
-	const val = input.value
-	if(val === '') return
-	console.log(val.length > max)
-	if(val.length < min)
-		showError(input,`${keyword}长度不能小于${min}`)
-	else if(val.length > max)
-		showError(input,`${keyword}长度不能大于${max}`)
-}
-function checkEquality(input1,input2,message){
-	console.log()
-	if(input1.value === input2.value)
-		showSuccess(input2)
-	else
-		showError(input2,message)
+
+	// 判断次数用尽
+	if (wrongLetters.length === partArr.length) {
+		finalMessage.innerText = "输入错误次数过多！游戏结束。"
+		popup.style.display = "flex"
+	}
 }
 
-
-function showError(input,message){
-	const formControl = input.parentElement
-	const small = formControl.querySelector('small')
-	small.innerText = message
-	formControl.classList.remove('success')
-	formControl.classList.add('error')
+// 再玩一次
+playAgain.onclick = () => {
+	correctLetters = []
+	wrongLetters = []
+	selectedWord = words[Math.floor(Math.random() * words.length)]
+	errLetter.innerHTML = ""
+	partArr.forEach(item => {
+		item.style.display = "none"
+	})
+	popup.style.display = "none"
+	displayWord()
 }
 
-function showSuccess(input){
-	const formControl = input.parentElement
-	formControl.classList.remove('error')
-	formControl.classList.add('success')
+// 监听键盘输入
+window.onkeydown = (e) => {
+	console.log(e.key)
+	if (e.keyCode >= 65 && e.keyCode <= 90) {
+		const letter = e.key
+		// 输入正确且不存在已经输入的字母中
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter)
+				displayWord()
+			} else {
+				showNote()
+			}
+		} else {
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter)
+				updateWrongLettersEl()
+			} else {
+				showNote()
+			}
+		}
+	}
 }

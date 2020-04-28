1fdf4e96908abe51defa73598e1fb4263ee99e08
yjl <545436317@qq.com>
2020-04-28 18:43:11 +0800

    打字游戏

--- index.js
+++ index.js
@@ -1,81 +1,114 @@
-// 获取节点
-const balance = document.getElementById("balance")
-const money_plus = document.getElementById("money-plus")
-const money_minus= document.getElementById("money-minus")
-const list = document.getElementById("list")
-const form = document.getElementById("form")
-const name = document.getElementById("name")
-const amount = document.getElementById("amount")
+const word = document.getElementById("word")
+const input = document.getElementById("input")
+const scoreEl = document.getElementById("score")
+const timeEl = document.getElementById("time")
+const endGame = document.getElementById("end-game-container")
+const settingBtn = document.getElementById("setting-btn")
+const settings = document.getElementById("setting")
+const difficultySelect = document.getElementById("difficulty")
 
-var records = []
-var money = {}
-
-function addTransactionDom(transaction){
-	const sign = transaction.amount < 0 ? "-" : "+"
-	const className = transaction.amount < 0 ? "minus" : "plus"
-	// 创建标签
-	const html = 
-	`
-		<li class="${className}">
-			${transaction.name}
-			<span>${sign}${Math.abs(transaction.amount)}</span>
-			<button class="delete-btn" onclick="deleteRecord(${transaction.id})">×</button>
-		</li>
-	`
-	let dom = document.createElement("div")
-	dom.innerHTML = html
-	dom = dom.children[0]
-	
-	list.appendChild(dom)
-	updateMoney(transaction)
+// 游戏单词
+const words = [
+	"steer",
+	"sliver",
+	"test",
+	"difficult",
+	"what",
+	"team"
+]
+var rightWords = []
+// 初始单词
+var randomWord
+// 初始得分
+var score = 0
+// 初始时间
+var time = 10
+// 难度对应时间
+var difficulty = {
+	easy: 5,
+	medium: 3,
+	hard: 2
 }
+var timer,diffTime
 
-// 更新余额/收入/支出
-function updateMoney(transaction){
-	money.balance += transaction.amount
-	transaction.amount < 0 ? money.minus+=transaction.amount : money.plus+=transaction.amount
-
-	// 更新dom
-	balance.innerText = `$${money.balance.toFixed(2)}`
-	money_plus.innerText = `+$${Math.abs(money.plus).toFixed(2)}`
-	money_minus.innerText = `-$${Math.abs(money.minus).toFixed(2)}`
-}
-
-// 删除
-function deleteRecord(id){
-	records.find((item,index) => {
-		if(item.id === id){
-			records.splice(index,1)
-			localStorage.setItem("records",JSON.stringify(records))
-			init()
-			return true
-		}
-	})
-}
-
-form.onsubmit = (e) => {
-	e.preventDefault()
-	const transaction = {id: +(new Date()),name: name.value,amount: Number(amount.value)}
-	records.push(transaction)
-	addTransactionDom(transaction)
-	name.value = ""
-	amount.value = ""
-	
-	localStorage.setItem("records",JSON.stringify(records))
-}
-
-// 初始化
+// 初始信息
 function init(){
-	if(localStorage.getItem("records"))
-		records = JSON.parse(localStorage.getItem("records"))
-	money = {
-		plus: 0,
-		minus: 0,
-		balance: 0,
-	}
-	list.innerHTML = ""
-	records.forEach(item => {
-		addTransactionDom(item)
-	})
+	settings.classList.remove("show")
+	settingBtn.classList.remove("active")
+	diffTime = difficulty[difficultySelect.value]
+	time = 10 
+	score = 0
+	updateAll()
+	input.focus()
+	endGame.style.display = "none"
+	timer = setInterval(() => {
+		time--
+		updateTime()
+		if(time <= 0){
+			clearInterval(timer)
+			gameOver()
+		}
+	},1000)
 }
-init()
+
+// 更新单词
+function addWordToDom(){
+	randomWord = getWord()
+	word.innerText = randomWord
+}
+// 获取单词
+function getWord(){
+	const newWords = words.filter(word => {
+		// 不包含
+		return rightWords.indexOf(word) === -1
+	})
+	const word = newWords[Math.floor(Math.random()*newWords.length)]
+	rightWords.push(word)
+	return word
+}
+// 更新时间
+function updateTime(){
+	timeEl.innerText = time + " s"
+}
+// 更新分数
+function updateScore(){
+	scoreEl.innerText = score
+}
+
+function updateAll(){
+	addWordToDom()
+	updateScore()
+	updateTime()
+}
+
+// 输入正确
+function inputRight(){
+	input.value = ""
+	time += diffTime
+	score += 1
+	updateAll()
+}
+
+// 游戏结束
+function gameOver(){
+	endGame.innerHTML = 
+	`
+		<h1>游戏结束</h1>
+		<p>您的最终得分: ${score}</p>
+		<button onclick="init()">再玩一次</button>
+	`
+	endGame.style.display = "flex"
+}
+
+input.oninput = (e) => {
+	const val = e.target.value
+	if(val === randomWord)
+		inputRight()
+}
+settingBtn.onclick = () => {
+	settings.classList.toggle("show")
+	settingBtn.classList.toggle("active")
+}
+difficultySelect.onchange = () => {
+	diffTime = difficulty[difficultySelect.value]
+}

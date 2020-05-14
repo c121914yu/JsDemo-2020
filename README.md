c34ecfb78a7ccdd4bf1748b129aa1a7c47998efa
yjl <545436317@qq.com>
2020-05-14 17:27:06 +0800

    歌词搜索

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# 3d旋转卡片
+# 歌词搜索
 
 ```css
-/* 父级 */
-transform-style: preserve-3d; // 启动3d
-perspective: 1000px; // 视距
+/* header内容的层级 */
+header *{
+	z-index: 1;
+}
+```
+```js
+// 接口地址
+const apiUrl = "https://api.lyrics.ovh"
+// 搜索接口
+"https://api.lyrics.ovh/suggest/" + (artist/title)
+// 歌词接口
+"https://api.lyrics.ovh/v1/artist/title"
+
+// 歌词换行
+const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g,'<br>')
+
+// 事件代理
+父级.onclick = (e) => {
+	e.target //子级元素
+}
+
+// 获取标签data
+<p name="11"></p>
+dom.getAttribute("name") //11
+
 
 ```
--- index.js
+++ index.js
@@ -1,124 +1,111 @@
-const card = document.querySelector(".card")
-const cardFont = card.querySelector(".card-front")
-const cardBack = card.querySelector(".card-back")
-const history = document.querySelector(".history")
+// dom
+const form = document.getElementById("form")
+const searchText = document.getElementById("search")
+const searchBtn = document.getElementById("search-btn")
+const result = document.getElementById("result")
+const more = document.getElementById("more")
 
-var words
-var wordIndex = 0
+const apiUrl = "https://api.lyrics.ovh"
 
-var synth = window.speechSynthesis
-var voices
-
-// 初始化speedAPi
-function getVoices(){
-	voices = synth.getVoices()
-	console.log(voices)
-	if(voices.length > 0)
-		getWords()
-}
-getVoices()
-if(synth.onvoiceschanged !== undefined)
-  synth.onvoiceschanged = getVoices
-	
-// 获取英文单词
-function getWords(){
-	let temp = localStorage.getItem("words")
-	if(temp){
-		randomWords(JSON.parse(temp))
-		updateContainer()
-	}
-	else
-	fetch("./word.json")
-	.then(res => res.json())
-	.then(data => {
-		randomWords(data)
-		localStort()
-		updateContainer()
-	})
-	
-	function randomWords(val){
-		words = val
-		words.sort(() => {
-			return Math.random() - Math.random()
-		})
-	}
-}
-
-// 切换单词
-function changeWord(index){
-	wordIndex += index
-	if(wordIndex < 0)
-		wordIndex = words.length-1
-	else if(wordIndex === words.length)
-		wordIndex = 0
-	// 清空正在播放的单词
-	synth.cancel()
-	updateContainer()
-}
-
-// 生/熟词切换
-function signWord(){
-	words[wordIndex].remenber = !words[wordIndex].remenber
-	if(words[wordIndex].remenber){
-		history.innerText = "熟词"
-		history.classList.add("remenber")
-	}
-	else{
-		history.innerText = "生词"
-		history.classList.remove("remenber")
-	}
-	localStort()
-}
-
-// 本地存储
-function localStort(){
-	localStorage.setItem("words",JSON.stringify(words))
-}
-
-function updateContainer(){
-	const word = words[wordIndex]
-	cardFont.innerHTML = 
-	`
-		<p>${word.word}</p>
-		<p>
-			${word.voice}
-			<i class="fa fa-volume-down" onclick="speakWord('${word.word}')"></i>
-		</p>
-	`
-	cardBack.innerHTML = 
-	`
-		<p>${word.translate}</p>
-	`
-	if(word.remenber){
-		history.innerText = "熟词"
-		history.classList.add("remenber")
-	}
-	else{
-		history.innerText = "生词"
-		history.classList.remove("remenber")
-	}
-	speakWord(word.word)
-}
-
-// 播放单词
-function speakWord(word){
-	if(voices.length === 0){
-		alert("你的浏览器不支持语言播放")
+form.onsubmit = (e) => {
+	e.preventDefault()
+	const searchTerm = searchText.value.trim()
+	if(!searchTerm){
+		alert("内容为空")
 		return
 	}
-	let speakText = new SpeechSynthesisUtterance(word)
-	speakText.lang = "en-US"
-	speakText.volume = 1
-	speakText.rate = 1
-	speakText.pitch = 1
 	
-	speakText.onerror = (err) => {
-		console.log(err)
-	}
-	synth.speak(speakText)
+	searchSong(searchTerm)
+}
+// 事件代理
+result.onclick = (e) => {
+	const dom = e.target
+	if(dom.tagName != "BUTTON") return
+	const artist = dom.getAttribute("data-artist")
+	const songTitle = dom.getAttribute("data-songtitle")
+	getLyrics(artist,songTitle)
 }
 
-card.onclick = () => {
-	if(synth.speaking)	return
-	card.classList.toggle("active")
+// 搜索歌词
+async function searchSong(term){
+	const res = await fetch(`${apiUrl}/suggest/${term}`)
+	const data = await res.json()
+	showData(data)
+}
+// 获取更多
+async function getMoreSongs(url){
+	const cors = 'https://cors-anywhere.herokuapp.com/'
+	const res = await fetch(cors + url)
+	const data = await res.json()
+	showData(data)
+}
+// 获取歌词
+async function getLyrics(artist,title){
+	const url = `${apiUrl}/v1/${artist}/${title}`
+	console.log(url)
+	const res = await fetch(url)
+	const data = await res.json()
+	const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g,'<br>')
+	result.innerHTML = 
+	`
+		<h2><strong>${artist}</strong> - ${title}</h2>
+		<span>${lyrics}</span>
+	`
+	more.innerHTML = ''
+}
+
+// 显示歌词信息dom
+function showData(data){
+	// 方法1
+	// let output = ''
+	// data.data.forEach(song => {
+	// 	output += `
+	// 		<li>
+	// 			<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+	// 			<button 
+	// 				class="btn"
+	// 				data-artist="${song.artist.name}"
+	// 				data-songtitle="${song.title}"
+	// 			>
+	// 				歌词
+	// 			</button>
+	// 		</li>
+	// 	`
+	// })
+	// result.innerHTML = 
+	// `
+	// 	<ul class="song">${output}</ul>
+	// `
+	
+	// 方法2
+	result.innerHTML = 
+	`
+		<ul class="song">
+			${data.data.map(song => 
+			`
+					<li>
+						<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+						<button 
+							class="btn"
+							data-artist="${song.artist.name}"
+							data-songtitle="${song.title}"
+						>
+							歌词
+						</button>
+					</li>
+			`).join("")}
+		</ul>
+	`
+	if(data.data.length === 0)
+		result.innerHTML = '<p>无数据</p>'
+	
+	if(data.prev || data.next){
+		more.innerHTML = 
+		`
+			${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">上一页</button>` : ''}
+			${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">下一页</button>` : ''}
+		`
+	}
+	else
+		more.innerHTML = ""
 }
